b558d049556cc0561fab79ca21737ff5686ae431
Pierre Henry <phenry@k2r3p6.42mulhouse.fr>
2026-09-16 18:33:38 +0200

    add: better prompt and test end json 2

--- src/algo.py
+++ src/algo.py
@@ -13,26 +13,34 @@ def decode_token_string(s: str) -> str:
 
 
 def format_prompt(prompt: str, tools: list[Tool]) -> str:
-    res = "Available functions:\n"
 
+    tools_list = ""
     for tool in tools:
-        param_str = ", ".join([f'''{p_name}: {str(p_type).split("'")[1]}''' for p_name, p_type in tool.parameters.items()])
-        res += f"- {tool.name}({param_str}): {tool.description}\n"
+        param_str = ", ".join([f'''{p_name}: {str(p_type).split("'")[1]}'''
+                               for p_name, p_type in tool.parameters.items()])
+        tools_list += f"- {tool.name}({param_str}): {tool.description}\n"
 
-    res += (
-        "\nYou are a function calling assistant who can only speak JSON."
-        "You must respond ONLY with a JSON object matching this structure: "
+    # return (
+    #     f"Available functions: \n{tools_list}"
+    #     "\nYou are a function calling assistant who can only speak JSON."
+    #     "You must respond ONLY with a JSON object matching this structure: "
+    #     '{"name": "<function_name>", "parameters": {<args>}}\n'
+    #     f"\nUser query: {prompt}\n"
+    #     "Find the appropriate function giving the user query, give its name and parameters as required by the JSON syntax.\n"
+    #     "Do not say anything outside of the JSON output.\n"
+    #     "Clean JSON output: {"
+    # )
+
+    return (
+        f"Available functions: \n{tools_list}"
+        "You are a function calling assistant.\n"
+        "Produce the output as a JSON with a top-level key called 'name' "
+        "and a second-level key named 'parameters' like this:\n"
         '{"name": "<function_name>", "parameters": {<args>}}\n'
-    )
-
-    res += (
-        f"\nUser query: {prompt}\n"
-        "Find the appropriate function giving the user query, give its name and parameters as required by the JSON syntax.\n"
-        "Do not say anything outside of the JSON output.\n"
+        "The output ends when the JSON is properly closed.\n"
+        f"User query: {prompt}\n"
         "Clean JSON output: {"
-        )
-    print(f"prompt: {res}", end ='')
-    return res
+    )
 
 def tensor_to_token(tensor: Tensor) -> list[int]:
     res = tensor.tolist() if hasattr(tensor, "tolist") else list(tensor)
@@ -47,6 +55,7 @@ def algo(prompt: str, tools: list[Tool]) -> str:
     model = Small_LLM_Model()
 
     prompt = format_prompt(prompt, tools)
+    print(f"promtp: {prompt}")
 
     current_input_ids = tensor_to_token(model.encode(prompt))
     init_ids = tensor_to_token(model.encode('{'))
@@ -66,8 +75,9 @@ def algo(prompt: str, tools: list[Tool]) -> str:
         if current_text.endswith("}}"):
             break
 
-    print("".join([model.decode(t) for t in generated_tokens]))
-    # return test_end_json("".join([model.decode(t) for t in generated_tokens]))
+    # print("".join([model.decode(t) for t in generated_tokens]))
+    return test_end_json2("".join([model.decode(t) for t in generated_tokens]))
+    # return "".join([model.decode(t) for t in generated_tokens])
 
 def test_end_json2(response: str):
     braces = 0
